c9b3bd7585bce094c375f54b1dfe9f69ab65f934
Abhishek Kumar <abhishek729299@gmail.com>
2022-10-03 19:12:33 +0530

    temperature.js file successfully uploaded

---
+++ temperature.js
@@ -0,0 +1,8 @@
+// Javascript program to convert celsius to farenheit
+
+const celsius = prompt("Enter a celsius value: ");
+
+let fahrenheit = (celsius * 1.8) + 32
+
+
+document.write(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
