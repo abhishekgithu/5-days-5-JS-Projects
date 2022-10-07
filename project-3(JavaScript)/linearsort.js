cd6089b5b6dd02197206dfdda0b235798d1a1794
Abhishek Kumar <abhishek729299@gmail.com>
2022-10-07 11:27:40 +0530

    linearsort.js file added successfully

---
+++ project-3(JavaScript)/linearsort.js
@@ -0,0 +1,15 @@
+const linear_Sort = (nums) => {
+    for (let i = 1; i < nums.length; i++) {
+      let j = i - 1
+      let temp = nums[i]
+      while (j >= 0 && nums[j] > temp) {
+        nums[j + 1] = nums[j]
+        j--
+      }
+      nums[j+1] = temp
+    }
+    return nums
+  }
+  console.log(linear_Sort([3, 0, 2, 5, -1, 4, 1]));
+  console.log(linear_Sort([2,6,5,12,-1,3,8,7,1,-4,0,23,1]));
+  
